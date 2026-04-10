8e14b5485d4e3d27da8c1fb445e57b56e2df8a2a
donaldiorwua <donaldiorwua@gmail.com>
2026-04-10 18:00:10 +0100

    Greeting commamd line programme

--- function/greeting.go
+++ function/greeting.go
@@ -6,6 +6,6 @@ func GreetingText() {
 	var input, mgs string
 	fmt.Println("Enter Your Name: ")
 	fmt.Scanln(&input)
-	mgs = "welcome to Go Lang!"
+	mgs = "welcome to learn2earn, where learning is turned into earning"
 	fmt.Println("Hello " + input + ", " + mgs)
 }
