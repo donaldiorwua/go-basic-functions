510ffa04163b074630cebbe11dfa6fed823ecddf
donaldiorwua <donaldiorwua@gmail.com>
2026-04-10 17:41:57 +0100

    String Transformer

--- function/go-reloaded.go
+++ function/go-reloaded.go
@@ -1,66 +1,67 @@
 package gobasicfunctions
 
 import (
+	"strconv"
 	"strings"
 )
 
-// func StringManipulator(word []string) []string {
-// 	var base int
-// 	var result []string
-// 	for _, words := range word {
-// 		index := len(result) - 1
-// 		switch words {
-// 		case "(oct)":
-// 			base = 8
-// 		case "(bin)":
-// 			base = 2
-// 		default:
-// 			base = 16
-// 		}
-// 		switch words {
-// 		case "(hex)", "(oct)", "(bin)":
-// 			num, err := strconv.ParseInt(result[index], base, 64)
-// 			if err == nil {
-// 				result[index] = strconv.FormatInt(num, 10)
-// 			}
-// 		case "(up)":
-// 			result[index] = strings.ToUpper(result[index])
-// 		case "(cap)":
-// 			result[index] = strings.Title(result[index])
-// 		case "(low)":
-// 			result[index] = strings.ToLower(result[index])
-// 		default:
-// 			result = append(result, words)
-
-// 		}
-// 	}
-// 	return result
-// }
-
-func textTransformer(words []string) []string {
+func StringManipulator(word []string) []string {
+	var base int
 	var result []string
-	index := len(result) - 1
-	for _, word := range words {
-		switch word {
+	for _, words := range word {
+		index := len(result) - 1
+		switch words {
+		case "(oct)":
+			base = 8
+		case "(bin)":
+			base = 2
+		default:
+			base = 16
+		}
+		switch words {
+		case "(hex)", "(oct)", "(bin)":
+			num, err := strconv.ParseInt(result[index], base, 64)
+			if err == nil {
+				result[index] = strconv.FormatInt(num, 10)
+			}
 		case "(up)":
-
 			result[index] = strings.ToUpper(result[index])
-
 		case "(cap)":
 			result[index] = strings.Title(result[index])
-
 		case "(low)":
 			result[index] = strings.ToLower(result[index])
 		default:
-			result = append(result, word)
+			result = append(result, words)
+
 		}
 	}
 	return result
 }
 
-func ToUpperN(word []string) []string {
-	for i := len(word) - 1; i > 0; i++ {
+// func textTransformer(words []string) []string {
+// 	var result []string
+// 	index := len(result) - 1
+// 	for _, word := range words {
+// 		switch word {
+// 		case "(up)":
 
-	}
-	return word
-}
+// 			result[index] = strings.ToUpper(result[index])
+
+// 		case "(cap)":
+// 			result[index] = strings.Title(result[index])
+
+// 		case "(low)":
+// 			result[index] = strings.ToLower(result[index])
+// 		default:
+// 			result = append(result, word)
+// 		}
+// 	}
+// 	return result
+// }
+
+// func ToUpperN(word []string) []string {
+// 	for i := len(word) - 1; i > 0; i++ {
+
+// 	}
+// 	return word
+// }
