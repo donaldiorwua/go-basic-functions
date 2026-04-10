d304a1353887f8926fdd873f27c5e5e16b3e8f5b
donaldiorwua <donaldiorwua@gmail.com>
2026-04-10 18:12:26 +0100

    Reading from Multiple files and writing to one file

--- function/processdir.go
+++ function/processdir.go
@@ -9,24 +9,24 @@ import (
 func ProcessDir() {
 	files, err := filepath.Glob("*.txt")
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error: ", err)
 		return
 	}
 	result, err := os.OpenFile("summary.log", os.O_WRONLY|os.O_APPEND, 0644)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error: ", err)
 	}
 	defer result.Close()
 
 	for _, file := range files {
 		content, err := os.ReadFile(file)
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println("Error: ", err)
 			continue
 		}
 		_, err = result.WriteString(string(content) + "\n")
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println("Error: ", err)
 		}
 	}
 	fmt.Println("Summary written successfully")
