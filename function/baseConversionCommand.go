afce24b731dc29b2386e9de1506438f286878908
donaldiorwua <donaldiorwua@gmail.com>
2026-04-11 02:51:45 +0100

    Updated Number Base Conversion

--- function/baseConversionCommand.go
+++ function/baseConversionCommand.go
@@ -2,14 +2,13 @@ package gobasicfunctions
 
 import (
 	"strconv"
-	"strings"
 )
 
 func BaseConversionCommand(word []string) []string {
 	var base int
 	var result []string
 
-	for i, words := range word {
+	for _, words := range word {
 		index := len(result) - 1
 		switch words {
 		case "(oct)":
@@ -25,18 +24,6 @@ func BaseConversionCommand(word []string) []string {
 			if err == nil {
 				result[index] = strconv.FormatInt(num, 10)
 			}
-		case "(up)":
-			result[index] = strings.ToUpper(result[index])
-		case "(cap)":
-			//if word[i] == "(cap)" {
-			char := word[i-1]
-			runeword := strings.Split(char, "")
-			word[i] = strings.ToUpper(runeword[0]) + strings.ToLower(strings.Join(runeword[1:], " "))
-			// } else {
-			// 	continue
-			// }
-		case "(low)":
-			result[index] = strings.ToLower(result[index])
 		default:
 			result = append(result, words)
 
